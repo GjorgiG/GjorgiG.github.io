285cb8ab65004ec1d9f6f8ea6566db0600dd168e
Gjorgi <20098721@mail.wit.ie>
2025-02-10 14:57:31 +0000

    Moved files into static folder for Flask + made requirements.txt to help with Render setup

--- stats.py
+++ stats.py
@@ -1,19 +1,29 @@
 from flask import Flask, jsonify
 from understatapi import UnderstatClient
 import asyncio
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+@app.route('/')
+def index():
+
+    return app.send_static_file('player-stats.html')
 
 @app.route('/get_data')
 def get_data():
     async def fetch_data():
         client = UnderstatClient()
-        # Test to get player stats from Prem 20/21
-        players = await client.get_league_players("EPL", 2021)
-        return players
+        
+        team_match_data = client.team(team="Manchester_United").get_match_data(season="2019")
+        
+        return team_match_data
 
-    # Run the async function in an event loop
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     data = loop.run_until_complete(fetch_data())
     return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
