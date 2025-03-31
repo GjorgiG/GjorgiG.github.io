3e3d9997a298787eabc850402867a2e5e2cde66c
Gjorgi <20098721@mail.wit.ie>
2025-03-31 14:27:43 +0100

    Trying out other API

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
-from understatapi import UnderstatClient
+from understat import Understat
 import asyncio
+import aiohttp
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static')
@@ -8,23 +9,21 @@ CORS(app)
 
 @app.route('/')
 def index():
-
     return app.send_static_file('player-stats.html')
 
 @app.route('/get_data')
 def get_data():
-    team = request.args.get('team', 'Manchester_United')
-    season = request.args.get('season', '2025')
+    team = request.args.get('team', 'Manchester United')
+    season = request.args.get('season', '2022')
 
     async def fetch_data(selected_team, selected_season):
-        client = UnderstatClient()
-        
-        return client.team(team=selected_team).get_match_data(season=selected_season)
+        async with aiohttp.ClientSession() as session:
+            understat = Understat(session)
+            data = await understat.get_team_games(selected_team, selected_season)
+            return data
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-
-    # Call the async function with user-selected team & season
     data = loop.run_until_complete(fetch_data(team, season))
     return jsonify(data)
 
